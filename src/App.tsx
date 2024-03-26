fc64f01783da2bd6f786d2366e78ce162e2f90b1
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-26 16:21:21 +0100

    Add projects component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import Header from "./components/Header"
-import Project from "./components/Project"
-import Todos from "./components/Todos"
-import ProjectForm from "./components/ProjectForm"
-import Projects from "./components/Projects"
+import Header from "./components/Header";
+import Project from "./components/Project";
+import Todos from "./components/Todos";
+import ProjectForm from "./components/ProjectForm";
+import Projects from "./components/Projects";
 import { useState } from "react";
 function App() {
-  const [isEditing,setIsEditing] = useState(false)
+  const [isEditing, setIsEditing] = useState(false);
+  const [projects, setProjects] = useState([])
   return (
     <>
-      <Header/>
-      <div className="flex flex-1 h-full border border-black">
-        {isEditing && <ProjectForm/>}
-        {!isEditing && <Project/>}
-        <Projects/>
-      <Todos/>
+      <Header />
+      <div className="flex flex-1 h-full">
+        <div className="w-[20%] flex flex-col flex-1 bg-neutral-500">
+        {isEditing && <ProjectForm />}
+        {!isEditing && <Project />}
+        {projects.map(project => <Projects/>)}
+        </div>
+        <Todos />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ProjectForm.tsx
+++ src/components/ProjectForm.tsx
@@ -1,7 +1,7 @@
 import Button from "./Button"
 const ProjectForm = () => {
   return (
-    <div className="bg-neutral-500 w-[20%] p-4">
+    <div className="bg-neutral-500 p-4">
     <input type="text" className="bg-neutral-200 flex justify-between cursor-pointer items-center p-3 px-6 w-[80%] text-2xl mx-auto mt-24 rounded-full hover:bg-neutral-300">
     </input>
     <div className="flex gap-4 w-[80%] mx-auto mt-2">
