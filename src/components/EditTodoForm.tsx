c99803a15b20ac4e9216f35a19ea1a15b7d48ce3
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-28 16:25:20 +0100

    Style editTodoForm

--- src/components/EditTodoForm.tsx
+++ src/components/EditTodoForm.tsx
@@ -1,33 +1,39 @@
 import Button from "./Button";
 const EditTodoForm = () => {
+  const proba = () => {
+    console.log("proba");
+  };
   return (
     <>
       <div className="w-[70%] mt-4">
-        <div className="p-4 flex justify-between text-xl rounded-lg items-center border border-red-500">
-          <div className="items-center flex gap-4">
-            <p>Title:</p>
+        <div className="p-4 flex justify-between text-xl rounded-lg items-center border border-neutral-500">
+          <div className="w-[50%] items-center justify-between flex gap-4">
+            <p>Todo:</p>
+            <input type="text" className="rounded-lg py-1 px-2 w-[80%]" />
           </div>
 
-          <div className="flex px-4 w-[70%] justify-between items-center">
+          <div className="flex px-4 gap-4 justify-between items-center">
             <p>Due Date: </p>
-
-            <div className="flex gap-4"></div>
-
+            <input type="date" className="bg-transparent" />
             <div className="flex items-center justify-end">
               <input
                 id="checked-checkbox"
                 type="checkbox"
                 checked={true}
-                //   onChange={() => toggleIsTodoFinished(id)}
+                onChange={proba}
                 className="w-4 h-4 text-blue-600 bg-neutral-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
               />
             </div>
           </div>
         </div>
-          <div className="flex p-4 border border-black">
-            <Button color={"bg-green-500"}>Save</Button>
-            <Button color={"bg-red-500"}>Cancel</Button>
-          </div>
+        <div className="flex py-4 gap-4">
+          <Button hoverColor={"hover:bg-green-600"} color={"bg-green-500"}>
+            Save
+          </Button>
+          <Button hoverColor={"hover:bg-red-600"} color={"bg-red-500"}>
+            Cancel
+          </Button>
+        </div>
       </div>
     </>
   );
